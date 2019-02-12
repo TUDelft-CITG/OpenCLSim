23c2c6c429ef1ea700ffe53415fcb56f6fc92f26
uijl <jorisdenuijl@gmail.com>
2019-02-12 08:39:28 +0100

    Attempt with new setup.py

--- setup.py
+++ setup.py
@@ -19,6 +19,44 @@ except VersionConflict:
     print("Error: version of setuptools is too old (<38.3)!")
     sys.exit(1)
 
+requires = [
+    "pandas",
+    "numpy",
+]
 
-if __name__ == "__main__":
-    setup(use_pyscaffold=True)
+tests_require = [
+    "pytest",
+    "pytest-cov",
+]
+
+
+setup(
+    name="digital_twin",
+    version="0.2.0",
+    description="The Digital Twin package aims to facilitate basic nautical traffic simulations.",
+    long_description="",  # README + '\n\n' + CHANGES,
+        classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    author="Mark van Koningsveld",
+    author_email="m.vankoningsveld@tudelft.nl",
+    url="https://github.com/TUDelft-CITG/digital_twin",
+    keywords="digital_twin",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    tests_require=tests_require,
+    install_requires=requires,
+    entry_points={
+        'console_scripts': [
+            'digital_twin=digital_twin.cli:main',
+        ],
+    },
+)
