2768f19cb425a8ca9b2effaa3f4af167a3f5dcdf
Fedor Baart <fedor.baart@deltares.nl>
2019-02-20 09:33:30 +0100

    allow to configure host and port

--- digital_twin/cli.py
+++ digital_twin/cli.py
@@ -10,17 +10,20 @@ import digital_twin.server
 
 @click.group()
 def cli(args=None):
-    """Console script for digital_twin."""
+    """Digital twin simulation."""
     click.echo("Replace this message by putting your code into "
                "digital_twin.cli.main")
     click.echo("See click documentation at http://click.pocoo.org/")
     return 0
 
 @cli.command()
+@click.option('--host', default='0.0.0.0')
+@click.option('--port', default=8080, type=int)
 @click.option('--debug/--no-debug', default=False)
-def serve(debug, args=None):
-    app = digital_twin.serve.app
-    app.run(host='0.0.0.0', debug=debug, port=80)
+def serve(host, port, debug, args=None):
+    """Run a flask server with the backend code"""
+    app = digital_twin.server.app
+    app.run(host=host, debug=debug, port=port)
 
 if __name__ == "__main__":
     sys.exit(main())  # pragma: no cover
