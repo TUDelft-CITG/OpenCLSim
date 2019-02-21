917eb4267ce2b21bdd6ee09e2db6aec358e2b7c9
Anne Hommelberg <anne.hommelberg@deltares.nl>
2019-02-21 13:32:01 +0100

    Add unit test for energy use
    Add a unit test which sends a simulation including energy use to the
    server.

--- tests/test_server.py
+++ tests/test_server.py
@@ -5,7 +5,7 @@ import numpy as np
 from digital_twin import server
 
 
-def run_and_compare_output(config_file, expected_result_file):
+def run_and_compare_completion_time(config_file, expected_result_file):
     with open(config_file) as f:
         config = json.load(f)
 
@@ -18,11 +18,12 @@ def run_and_compare_output(config_file, expected_result_file):
         expected_result = json.load(f)
 
     np.testing.assert_almost_equal(result["completionTime"], expected_result["completionTime"])
+    return result
 
 
 def test_move_activity():
     """Run a basic simulation containing a single move activity and check the output."""
-    run_and_compare_output(
+    run_and_compare_completion_time(
         config_file='tests/configs/move_activity.json',
         expected_result_file='tests/results/move_activity_result.json'
     )
@@ -30,28 +31,28 @@ def test_move_activity():
 
 def test_multiple_move_activities():
     """Run a basic simulation containing multiple move activities and check the output."""
-    run_and_compare_output(
+    run_and_compare_completion_time(
         config_file='tests/configs/multiple_move_activities.json',
         expected_result_file='tests/results/multiple_move_activities_result.json'
     )
 
 
 def test_single_run_activity():
-    run_and_compare_output(
+    run_and_compare_completion_time(
         config_file='tests/configs/single_run_activity.json',
         expected_result_file='tests/results/single_run_activity_result.json'
     )
 
 
 def test_multiple_single_run_activities():
-    run_and_compare_output(
+    run_and_compare_completion_time(
         config_file='tests/configs/multiple_single_run_activities.json',
         expected_result_file='tests/results/multiple_single_run_activities_result.json'
     )
 
 
 def test_conditional_activity():
-    run_and_compare_output(
+    run_and_compare_completion_time(
         config_file='tests/configs/conditional_activity.json',
         expected_result_file='tests/results/conditional_activity_result.json'
     )
@@ -61,7 +62,7 @@ def test_mover_properties_engine_order():
     """Run a basic simulation containing a single move activity and check the output.
     We give an engine order of 0.8 instead of the default 1.0 used for test_move_activity.
     The completionTime of this test's results should be 25% slower than that of test_move_activity."""
-    run_and_compare_output(
+    run_and_compare_completion_time(
         config_file='tests/configs/mover_properties_engine_order.json',
         expected_result_file='tests/results/mover_properties_engine_order_result.json'
    )
@@ -71,7 +72,22 @@ def test_mover_properties_load():
     """Run a basic conditional simulation.
     We order to load up to 0.8. Capacity of the ship is 2500 and 10.000 units need to be transported.
     Without the load order this would take 4 trips, it should now take 5."""
-    run_and_compare_output(
+    run_and_compare_completion_time(
         config_file='tests/configs/mover_properties_load.json',
         expected_result_file='tests/results/mover_properties_load_result.json'
     )
+
+
+def test_energy_use():
+    """Run a simulation tracking energy use."""
+    result = run_and_compare_completion_time(
+        config_file='tests/configs/energy_use.json',
+        expected_result_file='tests/results/energy_use_result.json'
+    )
+    hopper_logs = result["equipment"][0]["features"]
+    energy_use = 0
+    for log_entry in hopper_logs:
+        if "Energy use" in log_entry["properties"]["message"]:
+            energy_use += log_entry["properties"]["value"]
+
+    np.testing.assert_almost_equal(energy_use, 471440.2025916437)
