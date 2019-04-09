b45fa16a0c6062376b5cc6c67031518cd466b24c
Fedor Baart <fedor.baart@deltares.nl>
2019-04-09 10:36:15 +0200

    bump

--- setup.py
+++ setup.py
@@ -79,6 +79,6 @@ setup(
     test_suite="tests",
     tests_require=tests_require,
     url="https://github.com/TUDelft-CITG/digital_twin",
-    version="0.2.0",
+    version="0.3.0",
     zip_safe=False,
 )
