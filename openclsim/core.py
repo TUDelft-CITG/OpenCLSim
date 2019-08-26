e11007110be1cf00044fe62b342bc734b9a91d2a
Joris den Uijl <jorisdenuijl@gmail.com>
2019-08-26 09:51:53 +0200

    Small error fix

--- openclsim/core.py
+++ openclsim/core.py
@@ -1094,6 +1094,9 @@ class HasDepthRestriction:
         # If a filling degee has been specified
         if self.filling is not None:
             return self.filling * self.container.capacity / 100
+        
+        elif destination.name not in self.depth_data.keys():
+            return self.container.capacity
 
         # If not, try to optimize the load with regard to the tidal window
         else:
