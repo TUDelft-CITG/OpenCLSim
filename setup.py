6a548682178556f47ff0aa6c0c66f4d4a37cd0b8
Joris den Uijl <jorisdenuijl@gmail.com>
2020-03-27 09:44:33 +0100

    Prepare fore release 1.2.1

--- setup.py
+++ setup.py
@@ -79,6 +79,6 @@ setup(
     test_suite="tests",
     tests_require=tests_require,
     url="https://github.com/TUDelft-CITG/openclsim",
-    version="1.2.0",
+    version="1.2.1",
     zip_safe=False,
 )
