3bf832980536014da8b6cf48a5e890aa7cacd498
Pieter van Halem <Pieter.vanHalem@vanoord.com>
2020-06-16 11:56:10 +0200

    version v1.3.0a14

--- setup.py
+++ setup.py
@@ -68,6 +68,6 @@ setup(
     test_suite="tests",
     tests_require=tests_require,
     url="https://github.com/TUDelft-CITG/openclsim",
-    version="v1.3.0a12",
+    version="v1.3.0a14",
     zip_safe=False,
 )
