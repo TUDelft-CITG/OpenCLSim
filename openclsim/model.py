c3e24d374ed9b161803865bc1147f3e0a21fa021
Pieter van Halem <pieter.vanhalem@vanoord.com>
2020-08-10 09:30:37 +0200

    add repeat activity

--- openclsim/model.py
+++ openclsim/model.py
@@ -409,6 +409,49 @@ class WhileActivity(GenericActivity):
         self.register_process(main_proc=main_proc, show=self.print)
 
 
+class RepeatActivity(GenericActivity):
+    """The RepeatActivity Class forms a specific class for executing multiple activities in a dedicated order within a simulation.
+    The while activity is a structural activity, which does not require specific resources.
+
+    Parameters
+    ----------
+
+    sub_process
+        the sub_process which is executed in every iteration
+    repetitions
+        Number of times the subprocess is repeated    
+    start_event
+        the activity will start as soon as this event is triggered
+        by default will be to start immediately
+    """
+
+    def __init__(self, sub_process, repetitions: int, show=False, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        """Initialization"""
+
+        self.print = show
+        self.sub_process = sub_process
+        if not self.sub_process.postpone_start:
+            raise Exception(
+                f"In While activity {self.name} the sub_process must have postpone_start=True"
+            )
+        self.repetitions = repetitions
+        if not self.postpone_start:
+            self.start()
+
+    def start(self):
+        main_proc = partial(
+            repeat_process,
+            name=self.name,
+            sub_process=self.sub_process,
+            repetitions=self.repetitions,
+            requested_resources=self.requested_resources,
+            keep_resources=self.keep_resources,
+            activity=self,
+        )
+        self.register_process(main_proc=main_proc, show=self.print)
+
+
 class ShiftAmountActivity(GenericActivity):
     """The ShiftAmountActivity Class forms a specific class for shifting material from an origin to a destination.
     It deals with a single origin container, destination container and a single processor
@@ -590,6 +633,83 @@ def conditional_process(
     ii = 0
     while (not condition_event.processed) and ii < max_iterations:
         # for sub_process_ in (proc for proc in [sub_process]):
+        activity_log.log_entry(
+            f"sub process {sub_process.name}",
+            env.now,
+            -1,
+            None,
+            activity_log.id,
+            core.LogState.START,
+        )
+        sub_process.start()
+        yield from sub_process.call_main_proc(activity_log=activity_log, env=env)
+        sub_process.end()
+        activity_log.log_entry(
+            f"sub process {sub_process.name}",
+            env.now,
+            -1,
+            None,
+            activity_log.id,
+            core.LogState.STOP,
+        )
+        # work around for the event evaluation
+        # this delay of 0 time units ensures that the simpy environment gets a chance to evaluate events
+        # which will result in triggered but not processed events to be taken care of before further progressing
+        # maybe there is a better way of doing it, but his option works for now.
+        yield env.timeout(0)
+        ii = ii + 1
+
+    args_data["start_preprocessing"] = start_time
+    args_data["start_activity"] = start_while
+    activity.post_process(**args_data)
+
+    activity_log.log_entry(
+        f"conditional process {name}",
+        env.now,
+        -1,
+        None,
+        activity_log.id,
+        core.LogState.STOP,
+    )
+
+
+def repeat_process(
+    activity_log,
+    env,
+    repetitions,
+    sub_process,
+    name,
+    requested_resources,
+    keep_resources,
+    activity,
+):
+    message = f"Repeat activity {name}"
+
+    start_time = env.now
+    args_data = {
+        "env": env,
+        "activity_log": activity_log,
+        "message": message,
+        "activity": activity,
+        "sub_process": sub_process,
+        "repetitions": repetitions,
+        "name": name,
+        "keep_resources": keep_resources,
+    }
+    yield from activity.pre_process(args_data)
+
+    start_while = env.now
+
+    activity_log.log_entry(
+        f"repeat process {name}",
+        env.now,
+        -1,
+        None,
+        activity_log.id,
+        core.LogState.START,
+    )
+    ii = 0
+    while ii < repetitions:
         activity_log.log_entry(
             f"sub process {sub_process.name}",
             env.now,
