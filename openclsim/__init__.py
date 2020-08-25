bd01c4a8c2bc2b7f4c990f96e2f35bca93ab36ce
Pieter van Halem <Pieter.vanHalem@vanoord.com>
2020-08-25 14:10:25 +0200

    bugg

--- openclsim/__init__.py
+++ openclsim/__init__.py
@@ -2,6 +2,10 @@
 
 """Top-level package for OpenCLSim."""
 
+import openclsim.model as model
+
 __author__ = """Mark van Koningsveld"""
 __email__ = "M.vanKoningsveld@tudelft.nl"
 __version__ = "1.2.2"
+__all__ = ["model"]
+
