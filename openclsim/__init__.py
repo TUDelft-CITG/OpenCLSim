d4d9628cc3a3df1b05bdb1439ebeb1a9d84c24db
Pieter van Halem <pieter.vanhalem@vanoord.com>
2020-09-28 15:50:50 +0200

    add version atribute

--- openclsim/__init__.py
+++ openclsim/__init__.py
@@ -1,5 +1,7 @@
 """Top-level package for OpenCLSim."""
 
+import pkg_resources
+
 import openclsim.core as core
 import openclsim.model as model
 import openclsim.plugins as plugins
@@ -7,3 +9,4 @@ import openclsim.plugins as plugins
 __author__ = """Mark van Koningsveld"""
 __email__ = "M.vanKoningsveld@tudelft.nl"
 __all__ = ["model", "plugins", "core"]
+__version__ = pkg_resources.get_distribution(__name__).version
