fbd88281bd5b68f630f9e81a84968e1abde54ffc
Pieter van Halem <pieter.vanhalem@vanoord.com>
2020-09-28 16:38:08 +0200

    add weather plugin

--- openclsim/__init__.py
+++ openclsim/__init__.py
@@ -1,6 +1,6 @@
 """Top-level package for OpenCLSim."""
 
-import pkg_resources
+# import pkg_resources
 
 import openclsim.core as core
 import openclsim.model as model
@@ -9,4 +9,4 @@ import openclsim.plugins as plugins
 __author__ = """Mark van Koningsveld"""
 __email__ = "M.vanKoningsveld@tudelft.nl"
 __all__ = ["model", "plugins", "core"]
-__version__ = pkg_resources.get_distribution(__name__).version
+# __version__ = pkg_resources.get_distribution(__name__).version
