c455f40e5558cedc7dbb12fdabf169c60213ae61
Pieter van Halem <Pieter.vanHalem@vanoord.com>
2020-09-29 08:45:42 +0200

    add plot directory

--- openclsim/__init__.py
+++ openclsim/__init__.py
@@ -5,8 +5,9 @@
 import openclsim.core as core
 import openclsim.model as model
 import openclsim.plugins as plugins
+import openclsim.plot as plot
 
 __author__ = """Mark van Koningsveld"""
 __email__ = "M.vanKoningsveld@tudelft.nl"
-__all__ = ["model", "plugins", "core"]
+__all__ = ["model", "plugins", "core", "plot"]
 # __version__ = pkg_resources.get_distribution(__name__).version
