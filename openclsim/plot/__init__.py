48aacb1bd99ab106786a94ebbeb7d0eee442de13
Halem J.P. van (Pieter) <Pieter.vanHalem@vanoord.com>
2020-10-29 11:24:12 +0100

    add stepchart plot

--- openclsim/plot/__init__.py
+++ openclsim/plot/__init__.py
@@ -1,5 +1,7 @@
 """Directory for the simulation plots."""
 
 from .vessel_planning import vessel_planning
+from .log_dataframe import get_log_dataframe
+from .step_chart import get_step_chart
 
-__all__ = ["vessel_planning"]
+__all__ = ["vessel_planning", "get_log_dataframe", "get_step_chart"]
---
+++ openclsim/plot/step_chart.py
@@ -0,0 +1,22 @@
+"""Get the step chart of the container levels."""
+
+import matplotlib.pyplot as plt
+from .log_dataframe import get_log_dataframe
+
+
+def get_step_chart(simulation_objects):
+    """Get the step chart of the container levels."""
+
+    fig = plt.figure()
+    for obj in simulation_objects:
+        df = get_log_dataframe(obj)
+        container_list = obj.container.container_list
+        for container in container_list:
+            container = f" {container}" if container != "default" else ""
+            plt.plot(
+                list(df["Timestamp"]),
+                list(df[f"container level{container}"]),
+                label=f"{obj.name}{container}",
+            )
+    plt.legend()
+    return fig
