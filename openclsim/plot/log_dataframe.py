0defda8507aa82c0a8bbc132ceba6f517b8104f3
Halem J.P. van (Pieter) <Pieter.vanHalem@vanoord.com>
2020-11-02 11:41:58 +0100

    PR requested change

--- openclsim/plot/log_dataframe.py
+++ openclsim/plot/log_dataframe.py
@@ -7,16 +7,16 @@ def get_log_dataframe(simulation_object, activities=[]):
     """Get the log of the simulation objects in a pandas dataframe."""
 
     id_map = {act.id: act.name for act in activities}
-    return (
-        pd.concat(
-            [
-                pd.DataFrame(simulation_object.log)[
-                    ["Timestamp", "ActivityID", "ActivityState"]
-                ],
-                pd.DataFrame(simulation_object.log["ObjectState"]),
-            ],
-            axis=1,
-        )
-        .replace(id_map)
-        .rename(columns={"ActivityID": "Activity"})
+    return pd.concat(
+        [
+            (
+                pd.DataFrame(simulation_object.log)
+                .filter(items=["ActivityID"])
+                .rename(columns={"ActivityID": "Activity"})
+                .replace(id_map)
+            ),
+            pd.DataFrame(simulation_object.log).filter(["Timestamp", "ActivityState"]),
+            pd.DataFrame(simulation_object.log["ObjectState"]),
+        ],
+        axis=1,
     )
