8a328f2816185441b13f3277344578e47872f836
Halem J.P. van (Pieter) <Pieter.vanHalem@vanoord.com>
2020-11-04 09:16:19 +0100

    clean-up

--- openclsim/plot/log_dataframe.py
+++ openclsim/plot/log_dataframe.py
@@ -7,11 +7,16 @@ def get_log_dataframe(simulation_object, activities=[]):
     """Get the log of the simulation objects in a pandas dataframe."""
 
     id_map = {act.id: act.name for act in activities}
+
+    df = (
+        pd.DataFrame(simulation_object.log)
+        .sort_values(by=["Timestamp"])
+        .sort_values(by=["Timestamp"])
+    )
     return pd.concat(
         [
             (
-                pd.DataFrame(simulation_object.log)
-                .filter(items=["ActivityID"])
+                df.filter(items=["ActivityID"])
                 .rename(columns={"ActivityID": "Activity"})
                 .replace(id_map)
             ),
