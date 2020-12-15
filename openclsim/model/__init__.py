e82247192946e113ba37d8baf5c83a00669e8d03
Halem J.P. van (Pieter) <Pieter.vanHalem@vanoord.com>
2020-12-15 11:39:56 +0100

    Isort

--- openclsim/model/__init__.py
+++ openclsim/model/__init__.py
@@ -2,14 +2,13 @@
 
 from .base_activities import AbstractPluginClass, GenericActivity, PluginActivity
 from .basic_activity import BasicActivity
+from .helpers import get_subprocesses, register_processes
 from .move_activity import MoveActivity
 from .parallel_activity import ParallelActivity
 from .sequential_activity import SequentialActivity
 from .shift_amount_activity import ShiftAmountActivity
 from .single_run_process import single_run_process
 from .while_activity import RepeatActivity, WhileActivity
-
-from .helpers import register_processes, get_subprocesses
 
 __all__ = [
     "AbstractPluginClass",
