c65352b604a2e0354a6d4998ba5a938062dc7ec3
Fedor Baart <fedor.baart@deltares.nl>
2021-02-09 09:44:13 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def setup_package():
     needs_sphinx = {"build_sphinx", "upload_docs"}.intersection(sys.argv)
     sphinx = ["sphinx"] if needs_sphinx else []
     setup(
-        setup_requires=["six", "pyscaffold>=2.5a0,<2.6a0"] + sphinx, use_pyscaffold=True
+        setup_requires=["six", "pyscaffold"] + sphinx, use_pyscaffold=True
     )
 
 
