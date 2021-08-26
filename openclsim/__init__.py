8ae515bdf5f0381ab549b4433df35d14886ab370
Pieter van Halem <pietervanhalem@hotmail.com>
2021-08-27 00:15:59 +0200

    add version back

--- openclsim/__init__.py
+++ openclsim/__init__.py
@@ -1,6 +1,6 @@
 """Top-level package for OpenCLSim."""
 
-# import pkg_resources
+import pkg_resources
 
 import openclsim.core as core
 import openclsim.model as model
@@ -11,4 +11,4 @@ __author__ = """Mark van Koningsveld"""
 __email__ = "M.vanKoningsveld@tudelft.nl"
 __version__ = "v1.4.2"
 __all__ = ["model", "plugins", "core", "plot"]
-# __version__ = pkg_resources.get_distribution(__name__).version
+__version__ = pkg_resources.get_distribution(__name__).version
