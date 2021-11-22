5dfa33ed18379c8202a6c7243db9f760b8510afe
Pieter van Halem <pietervanhalem@hotmail.com>
2021-11-22 17:06:02 +0100

    fix

--- openclsim/__init__.py
+++ openclsim/__init__.py
@@ -1,13 +1,8 @@
 """Top-level package for OpenCLSim."""
 
-import pkg_resources
-
 import openclsim.core as core
 import openclsim.model as model
 import openclsim.plot as plot
 import openclsim.plugins as plugins
 
-__author__ = """Mark van Koningsveld"""
-__email__ = "M.vanKoningsveld@tudelft.nl"
-__version__ = pkg_resources.get_distribution(__name__).version
 __all__ = ["model", "plugins", "core", "plot"]
