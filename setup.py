e76c8881bc73c49228a2734f46c7e6c777e7cab3
Pieter van Halem <pietervanhalem@hotmail.com>
2021-11-23 11:22:38 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,10 @@
-"""
-    Setup file for openclsim.
-    Use setup.cfg to configure your project.
-
-    This file was generated with PyScaffold 4.1.1.
-    PyScaffold helps you to put up the scaffold of your new Python project.
-    Learn more under: https://pyscaffold.org/
-"""
+#!/usr/bin/env python
 from setuptools import setup
 
+
+def setup_package():
+    setup(use_scm_version={"fallback_version": "999"})
+
+
 if __name__ == "__main__":
-    try:
-        setup(use_scm_version={"version_scheme": "no-guess-dev"})
-    except:  # noqa
-        print(
-            "\n\nAn error occurred while building the project, "
-            "please ensure you have the most updated version of setuptools, "
-            "setuptools_scm and wheel with:\n"
-            "   pip install -U setuptools setuptools_scm wheel\n\n"
-        )
-        raise
+    setup_package()
