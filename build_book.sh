f540c67d6b87eade2e80cf759158d4fc8b3b8738
Fedor Baart <fedor.baart@deltares.nl>
2022-07-08 16:07:56 +0200

    buid book

--- build_book.sh
+++ build_book.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # Copy the examples from the notebooks
-cp notebooks/Example* book/examples/
+cp notebooks/*.ipynb book/examples/
 # Copy the api docs from the docs folder
-cp docs/opentnsim.rst book/docs
+cp docs/openclsim.rst book/docs
 # Copy the Authors list from the root folder
 cp AUTHORS.rst book/docs/
 # Copy logos
