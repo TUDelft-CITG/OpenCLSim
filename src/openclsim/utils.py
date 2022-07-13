1e71c78aaa9fb73a0ceb67f26e7f924db1066dd0
Fedor Baart <fedor.baart@deltares.nl>
2022-07-13 21:36:57 +0200

    use src_path

--- src/openclsim/utils.py
+++ src/openclsim/utils.py
@@ -3,8 +3,8 @@ import pathlib
 import openclsim
 
 
-def find_notebook_path():
-    """Lookup the path where the notebooks are located. Returns a pathlib.Path object."""
+def find_src_path():
+    """Lookup the path where the package are located. Returns a pathlib.Path object."""
     openclsim_path = pathlib.Path(openclsim.__file__)
     # check if the path looks normal
     assert "openclsim" in str(
@@ -12,5 +12,11 @@ def find_notebook_path():
     ), "we can't find the openclsim path: {openclsim_path} (openclsim not in path name)"
     # src_dir/openclsim/__init__.py -> ../.. -> src_dir
     src_path = openclsim_path.parent.parent
+    return src_path
+
+
+def find_notebook_path():
+    """Lookup the path where the notebooks are located. Returns a pathlib.Path object."""
+    src_path = find_src_path()
     notebook_path = src_path / "notebooks"
     return notebook_path
