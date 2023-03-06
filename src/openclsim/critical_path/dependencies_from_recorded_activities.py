4952f92a700bb1cfca432e882484e95512666e35
motl <luke.moth@witteveenbos.com>
2023-03-06 10:48:27 +0100

    first test and functionality for scope 4

--- src/openclsim/critical_path/dependencies_from_recorded_activities.py
+++ src/openclsim/critical_path/dependencies_from_recorded_activities.py
@@ -16,10 +16,30 @@ class DependenciesFromRecordedActivities(BaseCP):
         """
         Get dependencies directly from recorded activities (match on identical timestamps)
 
+        This method determines dependencies between activities 'blindly' based
+        on the simulation logs only. That is, purely based on timestamp and
+        simulation object.
+
+        The procedure of determining dependencies is as follows:
+
+        1. Standard dependencies are those that have matching end and start
+           times, and share at least one object.
+        2. If a WAITING activity is logged, then it is assumed that the object
+           is purposefully waiting for something. Hence, this should be
+           included in the dependencies.
+
         Notes
         -----
-        this method is likely to provide too many dependencies
-        (and hence make too many activities interconnected)
+            This method has a few known limitations which are listed below:
+            - It is a rather grand assumption to conclude dependency from the
+              log, i.e. matching time and location, only. As a result cases
+              where 'being at the same place at the same time' may be wrongly
+              interpreted as a dependency between activities.
+            - It is assumed that there is always a common object for a
+              dependency to occur. There are situations in which this may not
+              be the case, for instance some level dependencies.
+            - No explicit logic from the simulation model setup and objects can
+              be extracted from the logbooks (after simulation)
 
         Returns
         -------
@@ -27,5 +47,64 @@ class DependenciesFromRecordedActivities(BaseCP):
             list of tuples like [(A1, A2), (A1, A3), (A3, A4)]
             where A2 depends on A1 (A1 'causes' A2) et cetera
         """
-        # ...
-        return []
+        if self.dependency_list is None:
+            self.__set_dependency_list()
+
+        return self.dependency_list
+
+    def __set_dependency_list(self):
+        """
+        Hidden and protected method for the get_dependency_list.
+        """
+        # a recorded_activities_df is required
+        if self.recorded_activities_df is None:
+            self._make_recorded_activities_df()
+
+        dependency_list = []
+
+        # loop over each unique cp_activity
+        for cp_act in self.recorded_activities_df.loc[:, "cp_activity_id"].unique():
+            # and find cp_activities that END when this one STARTS
+            bool_this_activity = (
+                self.recorded_activities_df.loc[:, "cp_activity_id"] == cp_act
+            )
+            bool_end_when_start = (
+                self.recorded_activities_df.loc[:, "end_time"]
+                == self.recorded_activities_df.loc[
+                    bool_this_activity, "start_time"
+                ].iloc[0]
+            )
+            bool_shared_source = self.recorded_activities_df.loc[
+                :, "SimulationObject"
+            ].isin(
+                list(
+                    self.recorded_activities_df.loc[
+                        bool_this_activity, "SimulationObject"
+                    ]
+                )
+            )
+
+            # so standard dependencies requires identical time and (at least 1)
+            # shared Source Object
+            bool_dependencies = (
+                bool_shared_source & bool_end_when_start & ~bool_this_activity
+            )
+            if sum(bool_dependencies) > 0:
+                dependencies = self.recorded_activities_df.loc[bool_dependencies, :]
+                if "WAITING" in dependencies.loc[:, "state"].tolist():
+                    # This activity depends on waiting meaning: current
+                    # activity might have been waiting on activity of other
+                    # source object! We ASSUME that every activity of every
+                    # source object with identical end time of this WAITING is
+                    # a dependency for current activity
+                    dependencies = self.recorded_activities_df.loc[
+                        bool_end_when_start & ~bool_this_activity, :
+                    ]
+
+                # get all unique activities on which current cp_act depends
+                activities_depending = dependencies.loc[:, "cp_activity_id"].unique()
+
+                for act_dep in activities_depending:
+                    dependency_list.append((act_dep, cp_act))
+
+        self.dependency_list = dependency_list
---
+++ tests/test_critical_dependencies_from_recorded_activities.py
@@ -0,0 +1,30 @@
+"""
+Tests for openclsim.critical_path.dependencies_from_recorded_activities module
+"""
+from openclsim.critical_path.dependencies_from_recorded_activities import (
+    BaseCP,
+    DependenciesFromRecordedActivities,
+)
+
+
+def test_init(simulation_2_barges):
+    """Test initialisation."""
+    my_cp = DependenciesFromRecordedActivities(**simulation_2_barges)
+    assert hasattr(my_cp, "get_dependency_list") and callable(
+        getattr(my_cp, "get_dependency_list")
+    ), "this method 'get_dependency_list' should exist!"
+    assert issubclass(
+        DependenciesFromRecordedActivities, BaseCP
+    ), "should be child of BaseCP"
+
+
+def test_get_dependency_list(simulation_2_barges):
+    """test get dependency list"""
+    my_cp = DependenciesFromRecordedActivities(**simulation_2_barges)
+    dependency_list = my_cp.get_dependency_list()
+    assert len(dependency_list) == 113, "113 dependencies expected"
+    assert len(set(dependency_list)) == 113, "113 (non duplicate) dependencies expected"
+    cp_ids = {item for dependecy_tuple in dependency_list for item in dependecy_tuple}
+    assert cp_ids.issubset(
+        set(my_cp.recorded_activities_df.cp_activity_id)
+    ), "activity IDs must exist in recored_activities_df"
