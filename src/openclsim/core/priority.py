ac3c00ca26f4d2d6f0c2c324083240e57b2e6b6e
arash.sepehri <arashsepehri72@gmail.com>
2023-04-05 10:54:19 +0200

    Update Priority Mixin

--- src/openclsim/core/priority.py
+++ src/openclsim/core/priority.py
@@ -1,33 +1,34 @@
+import simpy
+
 class Priority:
-    def __init__(self, env, priority=0, *args, **kwargs):
-        super().__init__(env, *args, **kwargs)
-        self.priority = priority
+    def __init__(self):
+        self._priority = {}
 
-    def can_sail(self, location):
-        """Determine if the vessel can sail through the given location."""
-        priority_level = location.priority_level
-        return self.priority >= priority_level
+    def add_priority(self, type, priority):
+        self._priority[type] = priority
 
-    def enter_location(self, location):
-        """Request permission to enter the location based on priority."""
-        with location.request(priority=self.priority) as req:
-            yield req
+    def get_priority(self, type):
+        return self._priority[type]
 
-class Location:
-    def __init__(self, env, priority_level=0):
+
+class Berth:
+    def __init__(self, env):
         self.env = env
-        self.priority_level = priority_level
-        self.queue = []
+        self.soil = simpy.Resource(env, capacity=1)  # limited number of soil containers
+        self.cargo = simpy.Resource(env, capacity=1)  # limited number of cargo containers
+        self.priority = Priority()
 
-    def request(self, priority=0):
-        """Request permission to enter the location."""
-        req = self.env.event()
-        self.queue.append((priority, req))
-        self.queue.sort(reverse=True)
-        return req
+    def vessel(self, type):
+        if type == "dredging_vessel":
+            priority = 1  # dredging vessel has priority to claim soil
+            container = self.soil
+        elif type == "seagoing_vessel":
+            priority = 2  # seagoing vessel claims cargo container
+            container = self.cargo
+        else:
+            raise ValueError("Invalid vessel type.")
 
-    def allow_next(self):
-        """Allow the next vessel in the queue to enter the location."""
-        if len(self.queue) > 0:
-            _, req = self.queue.pop(0)
-            req.succeed()
+        with container.request(priority=self.priority.get_priority(type)) as request:
+            yield request
+            print(f"{type} claimed the {container.type} container.")
+            yield self.env.timeout(5 if type == "dredging_vessel" else 3)  # process container
