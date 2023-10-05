7faee261df379b54d8339c318518c86a0eafd1e7
Arash Sepehri <asepehri@tudelft.nl>
2023-10-05 17:28:22 +0200

    updates on access.py and notebook 33

--- src/openclsim/plugins/access.py
+++ src/openclsim/plugins/access.py
@@ -35,19 +35,37 @@ class DredgeCriterion:
 class DredgePluginActivity(openclsim.model.AbstractPluginClass):
     """Mixin for ShiftAmountActivity to initialize TestPluginShiftAmountActivity."""
 
-    def __init__(self, dredge_criteria=None):
+    def __init__(self, dredge_criteria=None, destination=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         assert isinstance(dredge_criteria, DredgeCriterion)
         self.dredge_criteria = dredge_criteria
+        self.destination = destination
 
     def pre_process(self, env, activity_log, activity, *args, **kwargs):
+        print("check dredging criteria", env.now)
+       
         if self.dredge_criteria is not None:
+            print("check if we need to dredge")
+            if self.check_constraint():
+                print("we are dredging")
+                activity_label = {
+                    "type": "plugin", "ref": "dredging"
+                }
+                waiting = 36000
+                return self.delay_processing(env, activity_label, activity_log, waiting)
             return {}
         else:
             return {}
 
     def check_constraint(self):
-        logger.info("Checking constraint in dredging plugin")
-        return []
+        logger.info("True if we need to dredge")
+        print(self.destination, self.destination.ABL, self.destination.DCL)
+        if self.destination.ABL > self.destination.DCL:
+            print("we are going to dredge")
+            return True
+        else:
+            print("we are not dredging")
+            return False
 
     def process_data(self, criterion) -> dict:
         result: dict = {}
@@ -57,11 +75,11 @@ class DredgePluginActivity(openclsim.model.AbstractPluginClass):
 class HasDredgePluginActivity(openclsim.model.PluginActivity):
     """Mixin for Activity to initialize DredgePluginActivity."""
 
-    def __init__(self, dredge_criteria, *args, **kwargs):
+    def __init__(self, dredge_criteria, destination, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         if dredge_criteria is not None and isinstance(
             self, openclsim.model.PluginActivity
         ):
-            dredge_plugin = DredgePluginActivity(dredge_criteria=dredge_criteria)
+            dredge_plugin = DredgePluginActivity(dredge_criteria=dredge_criteria, destination=destination)
             self.register_plugin(plugin=dredge_plugin, priority=2)
