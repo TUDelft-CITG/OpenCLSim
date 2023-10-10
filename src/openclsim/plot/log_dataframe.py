55595a0600426a2e53f9d36575b12c8bc867ad80
Gerben J. de Boer <gerbenjdeboer@gmail.com>
2023-10-10 22:24:02 +0200

    allow mapping to be dict return by run

--- src/openclsim/plot/log_dataframe.py
+++ src/openclsim/plot/log_dataframe.py
@@ -6,7 +6,9 @@ from openclsim.model import get_subprocesses
 
 
 def get_log_dataframe(simulation_object, id_map=None):
-    """Get the log of the simulation objects in a pandas dataframe.
+    """Get the chronological log of one simulation object in a pandas dataframe.
+
+    result is sorted by Timestamp
 
     Parameters
     ----------
@@ -14,14 +16,17 @@ def get_log_dataframe(simulation_object, id_map=None):
         object from which the log is returned as a dataframe sorted by "Timestamp"
     id_map
         by default uuids are not resolved. id_map solves this at request:
-        * a list of top-activities of which also all sub-activities
+        * a list or dict of top-activities of which also all sub-activities
           will be resolved, e.g.: [while_activity]
         * a manual id_map to resolve uuids to labels, e.g. {'uuid1':'name1'}
     """
     if id_map is None:
         id_map = []
 
+    if isinstance(id_map, dict):
+        id_map = [*id_map.values()]    
     if isinstance(id_map, list):
+        # TO DO should in fact be recursive for nested activities
         id_map = {act.id: act.name for act in get_subprocesses(id_map)}
     else:
         id_map = id_map if id_map else {}
