589d7227cc7dcb56f416edf5bc2d50bd26ebea27
Fedor Baart <fedor.baart@deltares.nl>
2024-02-02 09:59:47 +0100

    add tidal window initial version

--- src/openclsim/plugins/__init__.py
+++ src/openclsim/plugins/__init__.py
@@ -3,7 +3,12 @@
 from .delay import DelayPlugin, HasDelayPlugin
 from .weather import HasWeatherPluginActivity, WeatherCriterion
 from .depth import HasDepthPluginActivity, DepthCriterion
-from .access import HasDredgePluginActivity, DredgeCriterion
+from .access import (
+    HasDredgePluginActivity,
+    DredgeCriterion,
+    HasTidePluginActivity,
+    TideCriterion,
+)
 
 __all__ = [
     "HasWeatherPluginActivity",
@@ -14,4 +19,6 @@ __all__ = [
     "DepthPlugin",
     "HasDredgePluginActivity",
     "DredgeCriterion",
+    "HasTidePluginActivity",
+    "TideCriterion",
 ]
--- src/openclsim/plugins/access.py
+++ src/openclsim/plugins/access.py
@@ -32,6 +32,27 @@ class DredgeCriterion:
         self.name = name
 
 
+class TideCriterion:
+    """
+    Used to add determine if a vessel can start sailing based on the tide
+
+    Parameters
+    ----------
+    ...
+
+    """
+
+    def __init__(
+        self,
+        name: str,
+        *args,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        """Initialization"""
+        self.name = name
+
+
 class DredgePluginActivity(openclsim.model.AbstractPluginClass):
     """Mixin for ShiftAmountActivity to initialize TestPluginShiftAmountActivity."""
 
@@ -80,6 +101,66 @@ class DredgePluginActivity(openclsim.model.AbstractPluginClass):
         return result
 
 
+class TidePluginActivity(openclsim.model.AbstractPluginClass):
+    """Mixin for ShiftAmountActivity to initialize ShiftAmountActivity."""
+
+    def __init__(
+        self,
+        tide_criteria=None,
+        destination=None,
+        vessel=None,
+        actual_water_level=None,
+        *args,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+
+        assert isinstance(tide_criteria, TideCriterion)
+        self.tide_criteria = tide_criteria
+        self.actual_water_level = actual_water_level
+        self.destination = destination
+
+    def pre_process(self, env, activity_log, activity, *args, **kwargs):
+        print("check tidal criteria", env.now)
+
+        if self.tide_criteria is not None:
+            print("wait until we tidal window allows to sail")
+            activity_label = {"type": "plugin", "ref": "tide"}
+            waiting = self.time_until_tide_allows()
+            print(f"waiting for {waiting} seconds")
+            return activity.delay_processing(env, activity_label, activity_log, waiting)
+        else:
+            return {}
+
+    def tide_allows(self):
+        logger.info("True if we tidal window allows to sail")
+        print(self.actual_water_level, self.destination.MBL)
+        available_water_depth = self.actual_water_level - self.destination.MBL
+        gross_ukc = available_water_depth - self.vessel.T
+
+        if gross_ukc > 0:
+            print("we have positive under keel clearance")
+            return True
+        else:
+            print("we do not have positive under keel clearance")
+            return False
+
+    def time_until_dredging_needed(self):
+        """compute how long until next dredging cycle starts"""
+        remaining_bed_level = self.destination.DCL - self.destination.ABL
+        print(f"remaining_bed_level: {remaining_bed_level}")
+        if remaining_bed_level < 0:
+            return 0
+
+        # m / m/s
+        remaining_duration = remaining_bed_level / self.destination.SR
+        return remaining_duration
+
+    def process_data(self, criterion) -> dict:
+        result: dict = {}
+        return result
+
+
 class HasDredgePluginActivity(openclsim.model.PluginActivity):
     """Mixin for Activity to initialize DredgePluginActivity."""
 
@@ -94,4 +175,17 @@ class HasDredgePluginActivity(openclsim.model.PluginActivity):
             dredge_plugin = DredgePluginActivity(
                 dredge_criteria=dredge_criteria, destination=destination
             )
+            self.register_plugin(plugin=dredge_plugin, priority=2)
+
+
+class HasTidePluginActivity(openclsim.model.PluginActivity):
+    """Mixin for Activity to initialize TidePluginActivity."""
+
+    def __init__(self, tide_criteria, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if tide_criteria is not None and isinstance(
+            self, openclsim.model.PluginActivity
+        ):
+            tide_plugin = TidePluginActivity(tide_criteria=tide_criteria)
             self.register_plugin(plugin=dredge_plugin, priority=2)
