d88378edcf5119e9314a06a9bc16a4ee54c6e9dd
Fedor Baart <fedor.baart@deltares.nl>
2024-03-19 09:41:03 +0100

    add numpy doc strings

--- src/openclsim/core/movable.py
+++ src/openclsim/core/movable.py
@@ -19,6 +19,14 @@ try:
 except ImportError:
     # redefine if needed
     def pairwise(iterable):  # type: ignore
+        """
+        pairwise function, based on py310.
+
+        Parameters
+        ----------
+        iterable: iterable
+            an iterable to generate sequential pairs from
+        """
         # pairwise('ABCDEFG') --> AB BC CD DE EF FG
         a, b = itertools.tee(iterable)
         next(b, None)
@@ -40,8 +48,10 @@ class Movable(Locatable, PerformsActivity, Log):
 
     Parameters
     ----------
-    v: speed, speed over ground of the object in m/s
-    engine_order: factor that determines how much of the speed is used.
+    v: float
+       speed, speed over ground of the object in m/s
+    engine_order: float
+       factor that determines how much of the speed is used.
     """
 
     def __init__(self, v: float = 1, engine_order: float = 1, *args, **kwargs):
@@ -63,6 +73,16 @@ class Movable(Locatable, PerformsActivity, Log):
 
         The moving step can be part of an activity. Set the `activity_id` to the
         movable object to have it recorded in the log and in the timeout value.
+
+        Parameters
+        ----------
+        destination: Locatable, optional
+            The destination of the move.
+        duration: float, optional
+            The duration of the move.
+        engine_order: float, optional
+            The engine order to use for the move.
+
 
         """
         if destination is None:
@@ -111,7 +131,17 @@ class Movable(Locatable, PerformsActivity, Log):
 
     @staticmethod
     def compute_distance(origin: shapely.Geometry, destination: shapely.Geometry):
-        """Determine the sailing distance based on great circle path from origin to destination."""
+        """
+        Determine the sailing distance based on great circle path from origin to destination.
+
+        Parameters
+        ----------
+
+        origin: shapely.geometry.Point
+            The origin of the move.
+        destination: shapely.geometry.Point
+            The destination of the move.
+        """
         orig = shapely.geometry.shape(origin)
         dest = shapely.geometry.shape(destination)
         _, _, distance = WGS84.inv(orig.x, orig.y, dest.x, dest.y)
@@ -120,7 +150,19 @@ class Movable(Locatable, PerformsActivity, Log):
     def compute_duration(
         self, origin: shapely.Geometry, destination: shapely.Geometry, engine_order=1.0
     ):
-        """Determine the duration based on great circle path from origin to destination."""
+        """
+        Determine the duration based on great circle path from origin to destination.
+
+        Parameters
+        ----------
+        origin: shapely.geometry.Point
+            The origin of the move.
+        destination: shapely.geometry.Point
+            The destination of the move.
+        engine_order: float
+            The engine order to use for the move.
+
+        """
         distance = self.compute_distance(origin, destination)
         return distance / (self.v * engine_order)
 
@@ -133,7 +175,7 @@ class ContainerDependentMovable(Movable, HasContainer):
 
     Parameters
     ----------
-    compute_v
+    compute_v: function
         a function that returns the current speed, given the fraction of the
         the container that is filled (in [0,1]), e.g.:
             lambda x: x * (v_full - v_empty) + v_empty
@@ -186,9 +228,17 @@ class MultiContainerDependentMovable(Movable, HasMultiContainer):
 
 
 class Routable(Movable, Locatable):
-    """Mixin class: Something with a route (networkx node list format)
-    route: a list of node ids (available on env.graph)
-    path: a linestring used to sail over
+    """
+    Mixin class: Something with a route (networkx node list format)
+
+    Parameters
+    ----------
+
+    route: list, optional
+        a list of node ids (available on env.graph)
+    path: shapely.geometry.LineString, optional
+        a linestring used to sail over
+
     """
 
     # one instance on the class
@@ -220,6 +270,18 @@ class Routable(Movable, Locatable):
         origin: shapely.Geometry,
         destination: shapely.Geometry,
     ):
+        """
+        Determine the sailing distance based on great circle path from origin to destination.
+        If a route is provided, the distance is computed over the route.
+
+        Parameters
+        ----------
+        origin: shapely.geometry.Point
+            The origin of the route or great circle.
+        destination: shapely.geometry.Point
+            The destination of the route or great circle.
+        """
+
         if self.path is not None:
             a = self.path.line_locate_point(origin)
             b = self.path.line_locate_point(destination)
@@ -260,7 +322,15 @@ class Routable(Movable, Locatable):
             raise ValueError(self.route, "is of unexpected type")
 
     def move_to_geometry(self, geometry: shapely.geometry.Point):
-        """move to geometry"""
+        """
+        Move to a geometry. Time spend is based on the great circle distance.
+
+        Parameters
+        ----------
+
+        geometry: shapely.geometry.Point
+            geometry to move to
+        """
         linestring = shapely.geometry.LineString([self.geometry, geometry])
         distance = WGS84.geometry_length(linestring)
         duration = self.v * distance
@@ -268,7 +338,17 @@ class Routable(Movable, Locatable):
         self.geometry = geometry
 
     def pass_linestring(self, geometry: shapely.geometry.LineString):
-        """Pass a linestring."""
+        """
+        Pass a linestring, move along the linestring. Duration is computed on the great circle distance.
+
+        Parameters
+        ----------
+
+        geometry: shapely.geometry.LineString
+            geometry to move over
+
+
+        """
         a = shapely.geometry.Point(geometry.coords[0])
         b = shapely.geometry.Point(geometry.coords[-1])
         assert isinstance(geometry, shapely.geometry.LineString)
@@ -282,8 +362,18 @@ class Routable(Movable, Locatable):
     def order_geometry(
         geometry: shapely.geometry.LineString, a: shapely.geometry.Point
     ):
-        """Make sure the linestring starts at a. If the end of the linestring is
-        closer to a than the start, the linestring is inverted."""
+        """
+        Make sure the linestring starts at a. If the end of the linestring is
+        closer to a than the start, the linestring is inverted.
+
+        Parameters
+        ----------
+
+        geometry: shapely.geometry.LineString
+            geometry to reorder according to starting point a
+        a: shapely.geometry.Point
+            starting point of the geometry
+        """
         start = shapely.geometry.Point(*geometry.coords[0])
         end = shapely.geometry.Point(*geometry.coords[-1])
         _, _, distance_from_start = WGS84.inv(start.x, start.y, a.x, a.y)
@@ -296,7 +386,12 @@ class Routable(Movable, Locatable):
         return new_geometry
 
     def move_over_route(self, route: List[str]):
-        """sail over the route, a list of nodes"""
+        """
+        Sail over the route, a list of nodes.
+
+        route: list
+            a list of nodes that are available on the network
+        """
         a = route[0]
         a_geometry = self.graph.nodes[a]["geometry"]
         yield from self.move_to_geometry(a_geometry)
